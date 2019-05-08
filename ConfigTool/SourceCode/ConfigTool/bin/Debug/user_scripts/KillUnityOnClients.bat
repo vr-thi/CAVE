8eb8f5c28aa980f960faeb45ea517a8fe794b012
ToJa <tomasz.janik@thi.de>
2019-05-08 11:34:38 +0200

    small correction

--- ConfigTool/SourceCode/ConfigTool/bin/Debug/user_scripts/KillUnityOnClients.bat
+++
@@ -1,4 +0,0 @@
-if [%1] NEQ [] (
-	for /l %%x in (1, 1, 8) do taskkill /S \\ic-%%x /U icuser /P icuser1 /IM %2.exe /T /F
-)
-::for /l %%x in (1, 1, 8) do %~dp0tools\PsExec.exe /S \\ic-%%x /U icuser /P icuser1 taskkill /IM Meshi /T /F
